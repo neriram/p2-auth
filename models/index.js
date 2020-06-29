f2aa557d29b9f66e3e03362034c3c729eaf79f32
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-06-29 14:54:31 -0700

    auth views written

--- models/index.js
+++ models/index.js
@@ -1,1 +1,8 @@
 //handle DB connection/middleware for apps
+'use strict';
+
+//import file system
+//import path 
+// initialize orm layer (sequelize)
+//import env variables
+//initialize db variable
