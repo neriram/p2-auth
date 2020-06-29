35af3a85a44ea4483e9ec9e5c497fd649e84b9ca
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-06-29 11:52:57 -0700

    added npm packages for middleware

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 require('dotenv').config();
 const Express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const helmet = require('helmet');
+const session = require('express-session');
+const flash = require('flash');
+//passport, and custom middleware, sequelize sesssions, 
 
 // app setup
 const app = Express();
@@ -9,9 +13,10 @@ app.use(Express.urlencoded({ extended: false}));
 app.use(Express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+app.use(require('morgan')('dev'));
+app.use(helmet());
 
 //ROUTES
-
 app.get('/', function(req, res) {
     //check to see if user is logged in
     res.render('index')
